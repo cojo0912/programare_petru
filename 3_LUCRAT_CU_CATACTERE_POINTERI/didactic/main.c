86196404f8af9b48f218302d2b014f95fe77efe4
Andrei <andrei.cojocaru@radcom.ro>
2022-09-28 21:34:27 +0300

    .

--- 3_LUCRAT_CU_CATACTERE_POINTERI/didactic/main.c
+++ 3_LUCRAT_CU_CATACTERE_POINTERI/didactic/main.c
@@ -5,7 +5,13 @@
 
 int main(void)
 {
-
+    char v[100];
+    memset(v, 0, 100);
+    printf("%d\n", strlen(v));
+    memcpy(v, "12345", 2);
+    
+    printf("%s", v);
+    
     return 0;
 }
 
--- 3_LUCRAT_CU_CATACTERE_POINTERI/tema/main.c
+++ 3_LUCRAT_CU_CATACTERE_POINTERI/tema/main.c
@@ -1,12 +1,53 @@
 #include <stdio.h>
 #include <string.h>
-#define MAXSIZE 10
+#define LENGTH 100
 
+void printString(char string[])
+{
+    for(int i=0; i<LENGTH;i++)
+    {
+        if(string[i]==0)
+            printf("\\0");
+        else 
+            printf("%c", string[i]);
+    }
+    printf("\n");
+}
 
+void insertName(char *startPos[], int *nrNames, char newName[])
+{
+    strcpy(startPos[*nrNames], newName);
+    startPos[*nrNames+1] = &startPos[*nrNames][strlen(newName)+1];
+    (*nrNames)++;
+}
+
+void printNames(char names[], char *startPos[], int nrNames)
+{
+    for(int i=0; i<nrNames; i++)
+    printf("%d %d %s\n", startPos[i]-names, strlen(startPos[i])+1,startPos[i]); 
+}
+
+//strcmp
 
 int main(void)
 {
+    char names[LENGTH];
+    memset(names, 0, LENGTH);
+    //strcpy(&names[49], "Asterix");
 
+    char* startPos[LENGTH];
+    memset(startPos, 0, LENGTH);
+    startPos[0] = &names[0];
+    int nrNames = 0;
+
+    insertName(startPos, &nrNames, "ANA");
+    printString(names);
+    insertName(startPos, &nrNames, "ION");
+    printString(names);
+    insertName(startPos, &nrNames, "GICA");
+    printString(names);
+    printNames(names, startPos, nrNames);
+    return 0;
 
     char cmd; 
     do {
